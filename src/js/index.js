b1b64a8928894becc276357284be0b51a6fe0cc1
Margaret <match14@yandex.ru>
2020-11-20 00:05:21 +0300

    header

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 import {$} from './common';
 
+// Открыть/Закрыть мобильное меню
+$('.js-open-menu').on('click', function(){
+	// $('.js-main-nav').addClass('open');
+	$('.js-body').addClass('no-scroll open-menu');
+});
+
+$('.js-close-menu').on('click', function(){
+	// $('.js-main-nav').removeClass('open');
+	$('.js-body').removeClass('no-scroll open-menu');
+});
+
+
 // $(window).scroll(function(){
 // 	if($(this).scrollTop()>300){
 // 		$('.js-move-up').addClass('visible');
